1877aa562035e64d8075b0a4a2cc37e3ad556e75
SuhrobDev <suhrobqayumov4@gmail.com>
2022-05-11 20:49:19 +0500

    Initial commit

---
+++ app/src/main/java/com/soul/mygooglemap/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.soul.mygooglemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//MD5: D7:73:6D:D7:11:EC:8A:E0:12:33:E7:3D:3E:90:C4:B3
+//SHA1: 6C:EA:1E:CF:61:73:8A:A9:4D:16:CB:27:A9:14:E0:53:D7:C9:9C:EE
+//SHA-256: 49:99:7A:BB:1C:7B:69:DE:A1:19:68:2B:25:A4:75:9D:DB:26:AB:28:39:75:46:13:05:CC:47:54:0D:7B:0E:6A
